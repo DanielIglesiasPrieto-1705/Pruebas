50667b61dffa7ca9b2fe69299d628580d5f96d39
DanielIglesiasPrieto-1705 <danieliglesiasprieto@gmail.com>
2025-12-01 11:34:35 +0100

    Pruebas

---
+++ Tema4/Pruebas/js/index.js
@@ -0,0 +1,28 @@
+let num1=0;
+let num2=0;
+
+function añadirPersonas() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let table_html = document.getElementById("contenido_tabla");
+
+    table_html.innerHTML+= "<tr><td>"+ nombre +"</td><td>"+ apellido +"</td></tr>";
+}
+/*-----------------------------------------------------------------------*/
+
+function sumar(){
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    result = document.getElementById("result")
+
+    let result_num = 
+}
+function resta(){
+    
+}
+function multiplicar(){
+    
+}
+function dividir(){
+    
+}
