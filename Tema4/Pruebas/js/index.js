37505fb3615d99e24a42f1cb730d2dd5b35d9a6b
DanielIglesiasPrieto-1705 <danieliglesiasprieto@gmail.com>
2025-12-01 13:00:05 +0100

    Calculadora terminada

--- Tema4/Pruebas/js/index.js
+++ Tema4/Pruebas/js/index.js
@@ -1,28 +1,61 @@
-let num1=0;
-let num2=0;
+let num1 = 0;
+let num2 = 0;
 
 function añadirPersonas() {
-    let nombre = document.getElementById("nombre").value;
-    let apellido = document.getElementById("apellido").value;
-    let table_html = document.getElementById("contenido_tabla");
+  let nombre = document.getElementById("nombre").value;
+  let apellido = document.getElementById("apellido").value;
+  let table_html = document.getElementById("contenido_tabla");
 
-    table_html.innerHTML+= "<tr><td>"+ nombre +"</td><td>"+ apellido +"</td></tr>";
+  table_html.innerHTML +=
+    "<tr><td>" + nombre + "</td><td>" + apellido + "</td></tr>";
 }
 /*-----------------------------------------------------------------------*/
 
-function sumar(){
-    num1 = document.getElementById("num1").value;
-    num2 = document.getElementById("num2").value;
-    result = document.getElementById("result")
+function sumar() {
+  num1 = parseInt(document.getElementById("num1").value);
+  num2 = parseInt(document.getElementById("num2").value);
+  if (isNaN(num1)) {
+    num1 = 0;
+  } else if (isNaN(num2)) {
+    num2 = 0;
+  }
+  result = document.getElementById("result");
 
-    let result_num = 
+  result.innerHTML = "El resultado es: " + (num1 + num2);
 }
-function resta(){
-    
+function resta() {
+      num1 = parseInt(document.getElementById("num1").value);
+  num2 = parseInt(document.getElementById("num2").value);
+  if (isNaN(num1)) {
+    num1 = 0;
+  } else if (isNaN(num2)) {
+    num2 = 0;
+  }
+  result = document.getElementById("result");
+
+  result.innerHTML = "El resultado es: " + (num1 - num2);
 }
-function multiplicar(){
-    
+function multiplicar() {
+      num1 = parseInt(document.getElementById("num1").value);
+  num2 = parseInt(document.getElementById("num2").value);
+  if (isNaN(num1)) {
+    num1 = 0;
+  } else if (isNaN(num2)) {
+    num2 = 0;
+  }
+  result = document.getElementById("result");
+
+  result.innerHTML = "El resultado es: " + (num1 * num2);
 }
-function dividir(){
-    
+function dividir() {
+      num1 = parseInt(document.getElementById("num1").value);
+  num2 = parseInt(document.getElementById("num2").value);
+  if (isNaN(num1)) {
+    num1 = 0;
+  } else if (isNaN(num2)) {
+    num2 = 0;
+  }
+  result = document.getElementById("result");
+
+  result.innerHTML = "El resultado es: " + (num1 / num2);
 }
