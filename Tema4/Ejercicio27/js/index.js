441be194bfbd5fd2cc8d527cbac68606c84b8306
DanielIglesiasPrieto-1705 <danieliglesiasprieto@gmail.com>
2025-12-09 11:08:36 +0100

    Ejercicio 27 terminado

---
+++ Tema4/Ejercicio27/js/index.js
@@ -0,0 +1,11 @@
+contarOvejitas();
+
+function contarOvejitas(){
+    var contador = 100;
+while (contador >= 0) {
+  document.writeln(`<p>${contador} Ovejitas</p>`);
+  contador--;
+}
+document.writeln(`<p>Se han contado todas las ovejas, vamos a dormir ya</p>`);
+
+}
