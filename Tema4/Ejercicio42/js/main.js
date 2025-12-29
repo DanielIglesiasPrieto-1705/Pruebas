b75d75c3e5aa6c7a343b8540ee09f8c16d0aa5cc
DanielIglesiasPrieto-1705 <danieliglesiasprieto@gmail.com>
2025-12-29 08:46:16 +0100

    Ejercicio 42 terminado

---
+++ Tema4/Ejercicio42/js/main.js
@@ -0,0 +1,6 @@
+let meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+
+for(var mes of meses){
+document.writeln(`<p>${mes}</p>`)
+}
+document.writeln("FELIZ AÑO NUEVO!!!!!!!!!!!!!!!");
