8c059df99bb7e9c34d93daacd8ff26376d7c51cf
DanielIglesiasPrieto-1705 <danieliglesiasprieto@gmail.com>
2026-01-07 09:59:18 +0100

    Ejercicios

---
+++ Tema5/Ejercicio2/js/main.js
@@ -0,0 +1,29 @@
+let error = document.getElementById("errores");
+errores.style.color = "red";
+let form = document.getElementById("formulario");
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellidos");
+let direccion = document.getElementById("direccion");
+form.addEventListener("submit", function (evt) {
+  let errores = [];
+  evt.preventDefault();
+  if (comprobarDatos(nombre)) {
+    errores.push("Debes de indicar el nombre");
+  }
+  if (comprobarDatos(apellido)) {
+    errores.push("Debes de indicar el apellido");
+  }
+  if (comprobarDatos(direccion)) {
+    errores.push("Debes de indicar la direccion");
+  }
+  error.innerHTML = errores.join("<br>");
+});
+
+function comprobarDatos(obj) {
+  let dato = obj.value;
+  let correcto = false;
+  if (dato === null || dato === "") {
+    correcto = true;
+  }
+  return correcto;
+}
